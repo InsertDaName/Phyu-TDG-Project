39dcbd3c9cfb0c90cbb988c7a7621634bda33481
Phyu <132349404+InsertDaName@users.noreply.github.com>
2025-07-22 13:54:35 +1200

    Overlay Search bar
    I successfully created the search bar but not functional. I still need to fix the placing cuz it is piling up with my links on navigation bar.

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function openSearch(){
+  document.getElementById("SearchOverlay").style.display="";
+}
+
+function closeSearch(){
+  document.getElementById("SearchOverlay").style.display="none";
+}
