64f377e98c02e2f2be73221dd228ca0e74954b16
Phyu <132349404+InsertDaName@users.noreply.github.com>
2025-07-25 14:09:11 +1200

    Updating The culture page to hae arow of image on festival section.
    Updating The culture page to hae arow of image on festival section.. The code is not still wokring

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function openSearch(){
-  document.getElementById("SearchOverlay").style.display="";
-}
-
-function closeSearch(){
-  document.getElementById("SearchOverlay").style.display="none";
-}
