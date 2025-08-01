6668b630e9b79a0d1e463cb6f535b71c466c8448
Phyu <132349404+InsertDaName@users.noreply.github.com>
2025-08-02 01:49:19 +1200

    Finish Index and About page
    Feat: I made the navigation sticky and added the scroll js to change the colour of the nav when scrolled down to specific section.
    
    Fixed: I fixed and added the link to my social media icons and resources.
    
    I changed the aesthetic of the social meida icons.
    
    Changed the dummy text to text with context on topic section.
    
    Home page and about page is done!

--- script.js
+++ script.js
@@ -27,25 +27,3 @@ new Swiper('.card-wrapper', {
     },
   }
 });
-
-let darkmode = localStorage.getItem('darkmode')
-  const toggleButton = document.getElementById('theme-toggle');
-
-  const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode', 'active')
-  }
-
-    const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
-  }
-
-  if(darkmode === "active") enableDarkmode()
-
-  toggleButton.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode')
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-  })
-//   const icon = document.getElementById('theme-icon');
-//   const body = document.body;
